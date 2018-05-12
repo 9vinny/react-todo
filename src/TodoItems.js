700b9ea9011a351f2a18a679d2407f428733f12e
bkfwebdev <bryant.feld@gmail.com>
2018-05-12 12:36:12 -0400

    finishing touches added

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,10 +1,19 @@
 import React, {Component} from "react";
+import FlipMove from "react-flip-move";
 
 class TodoItems extends Component {
+constructor (props){
+    super(props);
+    this.createTask = this.createTask.bind(this);
+}
 
+    delete(key){
+        this.props.delete(key);
+    }
     createTask(item){
 
-        return <li key={item.key}>{item.text}</li>
+        return <li onClick = {()=>this.delete(item.key)}
+                key = {item.key}>{item.text}</li>
             
         
     }
@@ -15,8 +24,10 @@ class TodoItems extends Component {
         var listItems = todoEntries.map(this.createTask);
 
         return (
-            <ul className = "theilst">
+            <ul className = "theList">
+            <FlipMove duration = {250} easing = "ease-out">
             {listItems}
+            </FlipMove>
             </ul>
         );
     }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,5 @@
 import React , {Component} from "react";
-import TodoItems from "./TodoItems";
+import TodoItems from "./TodoItems"; 
 import "./TodoList.css";
 class TodoList extends Component {
     constructor(props){
@@ -8,6 +8,7 @@ class TodoList extends Component {
             items:[]
         };
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
 
     addItem(e){
@@ -29,6 +30,18 @@ class TodoList extends Component {
 
     }
 
+    deleteItem(key){
+        var filteredItems = this.state.items.filter(function(item){
+            return (item.key !== key);
+
+        });
+
+        this.setState ({
+            items:filteredItems
+        });
+
+    }
+
     render(){
         return (
             <div className = "todoListMain">
@@ -38,7 +51,8 @@ class TodoList extends Component {
                 <button type="submit" >Add</button>
                 </form>
                 </div>
-                <TodoItems entries={this.state.items}/>
+                <TodoItems entries={this.state.items}
+                delete = {this.deleteItem}/>
             </div>
         );
     }
